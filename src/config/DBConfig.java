ce4431b339ecdf0029a7bef973f2060baf4daf39
Matheus Antonio Oliveira Cardoso <matheus.antonio.c@outlook.com>
2019-01-14 19:01:25 -0200

    DBCondig estendido para o mongo
    Novo método para conexão ao banco através do Mongo.

--- src/config/DBConfig.java
+++ src/config/DBConfig.java
@@ -1,11 +1,11 @@
 package config;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.mongodb.MongoClient; 
+import com.mongodb.MongoClientURI;  
 
 public class DBConfig {
     
@@ -13,6 +13,10 @@ public class DBConfig {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/notas_db", "root", "root");
+    }
+
+    public static MongoClient getNoSqlConnection() {
+        return new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
     }
 
     
